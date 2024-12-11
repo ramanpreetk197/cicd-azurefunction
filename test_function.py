c498aa6111a50ba49cf7f0ab8df53cf522ee07ad
ramanpreetk197 <ramanpreetk197@gmail.com>
2024-12-10 23:32:29 -0500

    Update test_function.py

--- test_function.py
+++ test_function.py
@@ -1,7 +1,10 @@
+import pytest
+from unittest.mock import Mock
 import app
 
 def test_hello_world():
-    req = None  # Mock request object
+    req = Mock()  # Mock the HttpRequest
     response = app.main(req)
+    
     assert response.status_code == 200
     assert response.get_body() == b"Hello, World!"
